149438262ddbb6e60c16a9022cea91a81f5c1cb2
Rodney Woollett <rodney.woollett@gmail.com>
2025-01-31 17:44:50 +1300

    Init repo

---
+++ mnist.py
@@ -0,0 +1,47 @@
+'''
+Created on Oct 7, 2012
+
+@author: srmarsla
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pcn
+import pickle, gzip
+
+# Read the dataset in (code from sheet)
+f = gzip.open('mnist.pkl.gz','rb')
+train_set, valid_set, test_set = pickle.load(f, encoding='bytes')
+f.close()
+
+#lets plot the first digit in the training set
+plt.imshow(np.reshape(train_set[0][1,:],[28,28]))
+print("The correct digit for the first image in the train set is :", train_set[1][0])
+
+nread = 200
+# Just use the first few images
+train_in = train_set[0][:nread,:]
+
+# This is a little bit of work -- 1 of N encoding
+# Make sure you understand how it does it
+train_tgt = np.zeros((nread,10))
+for i in range(nread):
+    train_tgt[i,train_set[1][i]] = 1
+
+test_in = test_set[0][:nread,:]
+test_tgt = np.zeros((nread,10))
+for i in range(nread):
+    test_tgt[i,test_set[1][i]] = 1
+
+# Train a Perceptron on training set
+p = pcn.pcn(train_in, train_tgt)
+p.pcntrain(train_in, train_tgt,0.25,100)
+
+# This isn't really good practice since it's on the training data, 
+# but it does show that it is learning.
+p.confmat(train_in,train_tgt)
+
+# Now test it
+p.confmat(test_in,test_tgt)
+
+plt.show()
---
+++ pima.py
@@ -0,0 +1,71 @@
+
+# Code from Chapter 3 of Machine Learning: An Algorithmic Perspective (2nd Edition)
+# by Stephen Marsland (http://stephenmonika.net)
+
+# You are free to use, change, or redistribute the code in any way you wish for
+# non-commercial purposes, but please maintain the name of the original author.
+# This code comes with no warranty of any kind.
+
+# Stephen Marsland, 2008, 2014
+
+# Demonstration of the Perceptron on the Pima Indian dataset
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pcn
+
+pima = np.loadtxt('pima-indians-diabetes.data',delimiter=',')
+
+# Plot the first and second values for the two classes
+indices0 = np.where(pima[:,8]==0)
+indices1 = np.where(pima[:,8]==1)
+
+# plt.ioff()
+plt.plot(pima[indices0,4],pima[indices0,5],'go')
+plt.plot(pima[indices1,4],pima[indices1,5],'rx')
+
+# Perceptron training on the original dataset
+print("Output on original data")
+p = pcn.pcn(pima[:,:8],pima[:,8:9])
+p.pcntrain(pima[:,:8],pima[:,8:9],0.25,100)
+p.confmat(pima[:,:8],pima[:,8:9])
+
+# Various preprocessing steps
+#cap the maxium number of times pregnant to 8
+pima[np.where(pima[:,0]>8),0] = 8
+
+#Categorize the age data. <= 30 years is set to 1, 30-40 is set to 2, etc
+pima[np.where(pima[:,7]<=30),7] = 1
+pima[np.where((pima[:,7]>30) & (pima[:,7]<=40)),7] = 2
+pima[np.where((pima[:,7]>40) & (pima[:,7]<=50)),7] = 3
+pima[np.where((pima[:,7]>50) & (pima[:,7]<=60)),7] = 4
+pima[np.where(pima[:,7]>60),7] = 5
+
+
+#normalize all features
+pima[:,:8] = pima[:,:8]-pima[:,:8].mean(axis=0)
+pima[:,:8] = pima[:,:8]/pima[:,:8].var(axis=0)
+
+
+#print pima.mean(axis=0)
+#print pima.var(axis=0)
+#print pima.max(axis=0)
+#print pima.min(axis=0)
+
+#partition the training and test sets
+#select even numbered rows for training and odd rows for test set
+trainin = pima[::2,:8] #input features for training
+testin = pima[1::2,:8] #input features in the test set
+print(testin)
+traintgt = pima[::2,8:9] #corresponding class labels for the training set
+testtgt = pima[1::2,8:9] #corresponding class labels for the test set
+
+# Perceptron training on the preprocessed dataset
+print("Output after preprocessing of data")
+p1 = pcn.pcn(trainin,traintgt)
+p1.pcntrain(trainin,traintgt,0.25,100)
+p1.confmat(testin,testtgt)
+
+print(np.shape(testtgt), np.shape(np.where(testtgt[:,0] == 1)), np.shape(np.where(testtgt[:,0] == 0)))
+
+plt.show()
